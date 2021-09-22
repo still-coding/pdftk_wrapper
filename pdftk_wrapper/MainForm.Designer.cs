6e8d67b3721f4f2a7b905d712a0484f82676164f
still-coding <devitsyn.ivan.nikolaevich@gmail.com>
2021-09-23 04:33:37 +0600

    wip pdf page

--- pdftk_wrapper/MainForm.Designer.cs
+++ pdftk_wrapper/MainForm.Designer.cs
@@ -36,16 +36,35 @@ namespace pdftk_wrapper
             this.tslMessage = new System.Windows.Forms.ToolStripStatusLabel();
             this.tspbProgress = new System.Windows.Forms.ToolStripProgressBar();
             this.tslDetails = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tcFileTypes = new System.Windows.Forms.TabControl();
+            this.tpDoc = new System.Windows.Forms.TabPage();
+            this.btnDoc2Pdf = new System.Windows.Forms.Button();
+            this.tpPdf = new System.Windows.Forms.TabPage();
+            this.gbxRemove = new System.Windows.Forms.GroupBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txbRemoveRange = new System.Windows.Forms.TextBox();
+            this.lblRemoveRange = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.fswMainDir)).BeginInit();
             this.ssMain.SuspendLayout();
+            this.tcFileTypes.SuspendLayout();
+            this.tpDoc.SuspendLayout();
+            this.tpPdf.SuspendLayout();
+            this.gbxRemove.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // fswMainDir
             // 
             this.fswMainDir.EnableRaisingEvents = true;
             this.fswMainDir.SynchronizingObject = this;
-            this.fswMainDir.Changed += new System.IO.FileSystemEventHandler(this.fswMainDir_Changed);
+            this.fswMainDir.Changed += new System.IO.FileSystemEventHandler(this.fswMainDir_Changed_Deleted);
             this.fswMainDir.Deleted += new System.IO.FileSystemEventHandler(this.fswMainDir_Deleted);
+            this.fswMainDir.Renamed += new System.IO.RenamedEventHandler(this.fswMainDir_Changed_Deleted);
             // 
             // lvExplorer
             // 
@@ -55,17 +74,18 @@ namespace pdftk_wrapper
             this.lvExplorer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
             this.lvExplorer.HideSelection = false;
-            this.lvExplorer.Location = new System.Drawing.Point(12, 12);
+            this.lvExplorer.Location = new System.Drawing.Point(4, 3);
             this.lvExplorer.Name = "lvExplorer";
-            this.lvExplorer.Size = new System.Drawing.Size(639, 404);
+            this.lvExplorer.Size = new System.Drawing.Size(553, 407);
             this.lvExplorer.TabIndex = 0;
             this.lvExplorer.UseCompatibleStateImageBehavior = false;
             this.lvExplorer.View = System.Windows.Forms.View.Details;
             this.lvExplorer.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvExplorer_ColumnClick);
+            this.lvExplorer.SelectedIndexChanged += new System.EventHandler(this.lvExplorer_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "Файлы";
+            this.columnHeader1.Text = "Файлы (клик тут обращает выделение)";
             // 
             // ssMain
             // 
@@ -89,26 +109,156 @@ namespace pdftk_wrapper
             // 
             this.tspbProgress.Name = "tspbProgress";
             this.tspbProgress.Size = new System.Drawing.Size(100, 16);
+            this.tspbProgress.Step = 1;
+            this.tspbProgress.Visible = false;
             // 
             // tslDetails
             // 
             this.tslDetails.Name = "tslDetails";
             this.tslDetails.Size = new System.Drawing.Size(118, 17);
             this.tslDetails.Text = "toolStripStatusLabel2";
+            this.tslDetails.Visible = false;
+            // 
+            // tcFileTypes
+            // 
+            this.tcFileTypes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tcFileTypes.Controls.Add(this.tpDoc);
+            this.tcFileTypes.Controls.Add(this.tpPdf);
+            this.tcFileTypes.Location = new System.Drawing.Point(3, 3);
+            this.tcFileTypes.Name = "tcFileTypes";
+            this.tcFileTypes.SelectedIndex = 0;
+            this.tcFileTypes.Size = new System.Drawing.Size(321, 407);
+            this.tcFileTypes.TabIndex = 2;
+            this.tcFileTypes.Selected += new System.Windows.Forms.TabControlEventHandler(this.tcFileTypes_Selected);
+            // 
+            // tpDoc
+            // 
+            this.tpDoc.Controls.Add(this.btnDoc2Pdf);
+            this.tpDoc.Location = new System.Drawing.Point(4, 24);
+            this.tpDoc.Name = "tpDoc";
+            this.tpDoc.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDoc.Size = new System.Drawing.Size(313, 379);
+            this.tpDoc.TabIndex = 0;
+            this.tpDoc.Text = "DOC";
+            this.tpDoc.UseVisualStyleBackColor = true;
+            // 
+            // btnDoc2Pdf
+            // 
+            this.btnDoc2Pdf.Enabled = false;
+            this.btnDoc2Pdf.Location = new System.Drawing.Point(36, 28);
+            this.btnDoc2Pdf.Name = "btnDoc2Pdf";
+            this.btnDoc2Pdf.Size = new System.Drawing.Size(153, 23);
+            this.btnDoc2Pdf.TabIndex = 0;
+            this.btnDoc2Pdf.Text = "Конвертировать в PDF";
+            this.btnDoc2Pdf.UseVisualStyleBackColor = true;
+            this.btnDoc2Pdf.Click += new System.EventHandler(this.btnDoc2Pdf_Click);
+            // 
+            // tpPdf
+            // 
+            this.tpPdf.Controls.Add(this.textBox1);
+            this.tpPdf.Controls.Add(this.gbxRemove);
+            this.tpPdf.Location = new System.Drawing.Point(4, 24);
+            this.tpPdf.Name = "tpPdf";
+            this.tpPdf.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPdf.Size = new System.Drawing.Size(313, 379);
+            this.tpPdf.TabIndex = 1;
+            this.tpPdf.Text = "PDF";
+            this.tpPdf.UseVisualStyleBackColor = true;
+            // 
+            // gbxRemove
+            // 
+            this.gbxRemove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbxRemove.Controls.Add(this.btnRemove);
+            this.gbxRemove.Controls.Add(this.txbRemoveRange);
+            this.gbxRemove.Controls.Add(this.lblRemoveRange);
+            this.gbxRemove.Location = new System.Drawing.Point(3, 6);
+            this.gbxRemove.Name = "gbxRemove";
+            this.gbxRemove.Size = new System.Drawing.Size(301, 100);
+            this.gbxRemove.TabIndex = 3;
+            this.gbxRemove.TabStop = false;
+            this.gbxRemove.Text = "Удаление страниц";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(185, 58);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 0;
+            this.btnRemove.Text = "Удалить";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txbRemoveRange
+            // 
+            this.txbRemoveRange.Location = new System.Drawing.Point(172, 22);
+            this.txbRemoveRange.Name = "txbRemoveRange";
+            this.txbRemoveRange.Size = new System.Drawing.Size(100, 23);
+            this.txbRemoveRange.TabIndex = 2;
+            // 
+            // lblRemoveRange
+            // 
+            this.lblRemoveRange.AutoSize = true;
+            this.lblRemoveRange.Location = new System.Drawing.Point(26, 25);
+            this.lblRemoveRange.Name = "lblRemoveRange";
+            this.lblRemoveRange.Size = new System.Drawing.Size(140, 15);
+            this.lblRemoveRange.TabIndex = 1;
+            this.lblRemoveRange.Text = "с - по (или одно число):";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Location = new System.Drawing.Point(8, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvExplorer);
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tcFileTypes);
+            this.splitContainer1.Panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.splitContainer1.Size = new System.Drawing.Size(891, 413);
+            this.splitContainer1.SplitterDistance = 560;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(911, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.ssMain);
-            this.Controls.Add(this.lvExplorer);
             this.Name = "MainForm";
             this.Text = "Pdftk wrapper";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fswMainDir)).EndInit();
             this.ssMain.ResumeLayout(false);
             this.ssMain.PerformLayout();
+            this.tcFileTypes.ResumeLayout(false);
+            this.tpDoc.ResumeLayout(false);
+            this.tpPdf.ResumeLayout(false);
+            this.tpPdf.PerformLayout();
+            this.gbxRemove.ResumeLayout(false);
+            this.gbxRemove.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +273,16 @@ namespace pdftk_wrapper
         private System.Windows.Forms.ToolStripStatusLabel tslMessage;
         private System.Windows.Forms.ToolStripProgressBar tspbProgress;
         private System.Windows.Forms.ToolStripStatusLabel tslDetails;
+        private System.Windows.Forms.TabControl tcFileTypes;
+        private System.Windows.Forms.TabPage tpDoc;
+        private System.Windows.Forms.Button btnDoc2Pdf;
+        private System.Windows.Forms.TabPage tpPdf;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox gbxRemove;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txbRemoveRange;
+        private System.Windows.Forms.Label lblRemoveRange;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- pdftk_wrapper/MainForm.cs
+++ pdftk_wrapper/MainForm.cs
@@ -1,31 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Text;
+using System.Linq;
 using System.Drawing;
 using System.IO;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
 
 namespace pdftk_wrapper
 {
     public partial class MainForm : Form
     {
+        enum FileType { doc, pdf };
+
+        struct PageRange
+        {
+            public PageRange(uint start, uint end)
+            {
+                Start = start > 1 ? start : 1;
+                End = end > start? end : start;
+            }
+
+            public PageRange(uint start)
+            {
+                Start = start > 1 ? start : 1;
+                End = start;
+            }
+
+            public uint Start { get; }
+            public uint End { get; }
+
+            public bool Overlaps(PageRange other)
+            {
+                // this     other
+                //  []       ()
+                // this.end overlaps with other.start
+                //   [ (] )
+                if (this.End >= other.Start && this.Start < other.End)
+                    return true;
+
+                // this inside other
+                // ([])
+                if (this.Start >= other.Start && this.End <= other.End)
+                    return true;
+
+                // this.start overlaps with other.end
+                // ( [) ]
+                if (this.Start <= other.End && this.End > other.End)
+                    return true;
+
+                // other inside this
+                // [ () ]
+                if (this.Start <= other.Start && this.End >= other.End)
+                    return true;
+
+                return false;
+            }
+
+            public List<PageRange> SortOrFindOverlapping(List<PageRange> prList)
+            {
+                List<PageRange> res = new List<PageRange>();
+
+                for (int i = 0; i < prList.Count; i++)
+                {
+                    for (int j = 0; j < prList.Count; j++)
+                    {
+                        if (i == j)
+                            continue;
+                        if (prList[i].Overlaps(prList[j]))
+                            return res;
+                    }
+                }
+
+                prList.Sort((x, y) => x.Start.CompareTo(y.Start));
+                return prList;
+            }
+
+        }
+
         static string workingDirName = "рабочая";
-        string watchPath = Path.Combine(Application.StartupPath, workingDirName);
-        
+        string workingPath = Path.Combine(Application.StartupPath, workingDirName);
+
+        static string doneDirName = "готовые";
+        string donePath = Path.Combine(Application.StartupPath, doneDirName);
+
+        FileType currentFileType = FileType.doc;
         delegate void UpdateFilesDelegate(string path, string filter);
         UpdateFilesDelegate updDelegate;
 
-        private void updateFiles(string path, string filter)
+        void updateListViewFiles(string path, string filter)
         {
             ImageList imageList = new ImageList();
             lvExplorer.SmallImageList = imageList;
             lvExplorer.Items.Clear();
             foreach (string item in Directory.GetFiles(path, filter))
             {
-                if (path.StartsWith("~"))
+                if (item.Contains("~$"))
                     continue;
                 imageList.Images.Add(Icon.ExtractAssociatedIcon(item));
                 lvExplorer.Items.Add(new FileInfo(item).Name, imageList.Images.Count - 1);
@@ -33,34 +106,241 @@ namespace pdftk_wrapper
             lvExplorer.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
         }
 
+        void updateFiles()
+        {
+            lvExplorer.Invoke(updDelegate, workingPath, currentFileType == FileType.doc ? "*.doc?" : "*.pdf");
+        }
+
         public MainForm()
         {
             InitializeComponent();
-            updDelegate = new UpdateFilesDelegate(updateFiles);
+            updDelegate = new UpdateFilesDelegate(updateListViewFiles);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            fswMainDir.Path = Directory.CreateDirectory(watchPath).FullName;
-            lvExplorer.Invoke(updDelegate, watchPath, "*.doc?");
+            Directory.CreateDirectory(donePath);
+            fswMainDir.Path = Directory.CreateDirectory(workingPath).FullName;
+            updateFiles();
+
+            tslMessage.Text = $"Найдено файлов: {lvExplorer.Items.Count}";
+        }
+
+        private void fswMainDir_Changed_Deleted(object sender, EventArgs e)
+        {
+            updateFiles();
         }
 
         private void fswMainDir_Deleted(object sender, FileSystemEventArgs e)
         {
-            Directory.CreateDirectory(watchPath);
-            lvExplorer.Invoke(updDelegate, watchPath, "*.doc?");
-        }
-
-        private void fswMainDir_Changed(object sender, FileSystemEventArgs e)
-        {
-            lvExplorer.Invoke(updDelegate, watchPath, "*.doc");
+            Directory.CreateDirectory(workingPath);
+            updateFiles();
         }
 
         private void lvExplorer_ColumnClick(object sender, ColumnClickEventArgs e)
         {
             foreach (ListViewItem item in lvExplorer.Items)
-            {
                 item.Selected = !item.Selected;
+        }
+
+        private void btnDoc2Pdf_Click(object sender, EventArgs e)
+        {
+            int cnt = lvExplorer.SelectedItems.Count;
+            if (cnt == 0)
+                return;
+            int progressPos = 0;
+            tslMessage.Text = "Конвертация файлов...";
+            tspbProgress.Maximum = cnt;
+            tspbProgress.Value = 0;
+            tslDetails.Text = $"0/{cnt}";
+            tspbProgress.Visible = tslDetails.Visible = true;
+            Word.Application wordApp = new Word.Application();
+
+            foreach (ListViewItem item in lvExplorer.SelectedItems)
+            {
+                string file = Path.Combine(workingPath, item.Text);
+                Word.Document doc = wordApp.Documents.Open(file);
+                doc.ExportAsFixedFormat(Path.Combine(donePath, Path.GetFileNameWithoutExtension(file) + ".pdf"), Word.WdExportFormat.wdExportFormatPDF);
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                doc.Close(false);
+                Marshal.ReleaseComObject(doc);
+
+                // TODO make async
+                tspbProgress.PerformStep();
+                tslDetails.Text = $"{++progressPos}/{cnt}";
+                Application.DoEvents();
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            wordApp.Quit();
+            Marshal.ReleaseComObject(wordApp);
+            tslMessage.Text = "Готово";
+            tspbProgress.Visible = tslDetails.Visible = false;
+        }
+
+        private void lvExplorer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cnt = lvExplorer.SelectedItems.Count;
+            tslMessage.Text = $"Выделено файлов: {cnt}";
+            btnDoc2Pdf.Enabled = Convert.ToBoolean(cnt);
+        }
+
+        private void tcFileTypes_Selected(object sender, TabControlEventArgs e)
+        {
+            currentFileType = e.TabPage == tpDoc ? FileType.doc : FileType.pdf;
+            lvExplorer.Invoke(updDelegate, workingPath, currentFileType == FileType.doc ? "*.doc?" : "*.pdf");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            // TODO refactor this ugly shit
+            char dash = '-';
+            char comma = ',';
+
+            char[] arr = txbRemoveRange.Text.Where(c => (char.IsDigit(c) || c == dash || c == comma)).ToArray();
+            string range = new string(arr);
+            string str = range.Substring(0, range.IndexOf(dash) + 1);
+            range = str + range.Substring(str.Length).Replace(dash.ToString(), "");
+            string[] ranges = range.Split(comma);
+
+            bool wasError = false;
+            int maxpage = 0;
+            foreach (string r in ranges)
+            {
+                string[] pages = r.Split(dash);
+                
+                if (pages.Length == 2)
+                {
+                    if (int.Parse(pages[0]) >= int.Parse(pages[1]))
+                    {
+                        MessageBox.Show($"Неправильный диапазон: {r}", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        wasError = true;
+                    }
+                }
+
+                foreach (string p in pages)
+                {
+                    int page = int.Parse(p);
+                    if (page < 1)
+                    {
+                        MessageBox.Show($"Неправильный номер страницы: 0", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        wasError = true;
+                    }
+                    maxpage = page > maxpage ? page : maxpage;
+                }            
+            }
+
+            if (wasError)
+                return;
+
+            range = string.Join(' ', ranges);
+
+
+
+            //var process = new Process();
+            //var psi = new ProcessStartInfo();
+            //psi.FileName = "cmd.exe";
+            //psi.RedirectStandardInput = true;
+            //psi.RedirectStandardOutput = true;
+            //psi.RedirectStandardError = true;
+            //psi.UseShellExecute = false;
+            //psi.WorkingDirectory = Application.StartupPath;
+            //process.StartInfo = psi;
+            //process.Start();
+            //process.OutputDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            //process.ErrorDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            //process.BeginOutputReadLine();
+            //process.BeginErrorReadLine();
+            //using (StreamWriter sw = process.StandardInput)
+            //{
+            //    //if (sw.BaseStream.CanWrite)
+            //    //{
+            //        sw.WriteLine("chcp 65001");
+
+            //        foreach (ListViewItem item in lvExplorer.SelectedItems)
+            //        {
+            //            string file = Path.Combine(workingDirName, item.Text);
+            //            string newFile = Path.Combine(doneDirName, item.Text);
+
+            //            sw.WriteLine($".\\pdftk.exe \"{file}\" cat {range} {maxpage + 1}-end output \"{newFile}\"");
+            //            //sw.WriteLine("exit");
+
+            //        }
+            //    //}
+            //}
+            //process.WaitForExit();
+
+
+
+
+
+
+            //Process proc = new Process();
+            //ProcessStartInfo info = new ProcessStartInfo();
+            //info.FileName = "cmd.exe";
+            //info.RedirectStandardInput = true;
+            //info.UseShellExecute = false;
+            //proc.StartInfo = info;
+            //proc.Start();
+
+            //using (StreamWriter sw = proc.StandardInput)
+            //{
+            //    if (sw.BaseStream.CanWrite)
+            //    {
+            //        sw.WriteLine("chcp 65001");
+            //        sw.WriteLine($"cd \"{}\"");
+
+            //        foreach (ListViewItem item in lvExplorer.SelectedItems)
+            //        {
+            //            string file = Path.Combine(workingDirName, item.Text);
+            //            string newFile = Path.Combine(doneDirName, item.Text);
+
+            //            sw.WriteLine($".\\pdftk.exe \"{file}\" cat {range} {maxpage + 1}-end output \"{newFile}\"");
+            //            //sw.WriteLine("exit");
+
+            //        }
+            //    }
+            //}
+            foreach (ListViewItem item in lvExplorer.SelectedItems)
+            {
+            string file = Path.Combine(workingDirName, item.Text);
+
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.WorkingDirectory = Application.StartupPath;
+            p.StartInfo.Arguments = $"/c chcp 65001 && .\\pdftk.exe \"{file}\" dump_data";
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            
+                int pos = output.IndexOf("NumberOfPages");
+            string line = output.Substring(pos, output.Length - pos);
+                line = line.Substring(line.IndexOf(" ") + 1, line.IndexOf(Environment.NewLine) - line.IndexOf(" ") - 1);
+                MessageBox.Show("+" + line + "+");
+                int pageCount = int.Parse(line);
+
+                HashSet<int> pages = new HashSet<int>();
+                for (int i = 1; i <= pageCount; i++)
+                    pages.Add(i);
+
+
+            
+                //string file = Path.Combine(workingDirName, item.Text);
+                string newFile = Path.Combine(doneDirName, item.Text);
+
+                // pdftk in.pdf cat 1-12 14-end output out.pdf
+
+                string command = $"/c chcp 65001 && cd \"{Application.StartupPath}\" && .\\pdftk.exe \"{file}\" cat {range} {maxpage + 1}-end output \"{newFile}\"";
+
+                textBox1.Text = command;
+
+                Process.Start("cmd", command);
+
             }
         }
     }
